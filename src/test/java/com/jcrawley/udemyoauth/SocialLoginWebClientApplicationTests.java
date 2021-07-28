04c76e505b3edbd21a6864dad97fd8b97ac0e735
johncrawley <johncrawley1@gmail.com>
2021-07-28 22:23:10 +0100

    Initial Commit

---
+++ src/test/java/com/jcrawley/udemyoauth/SocialLoginWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcrawley.udemyoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
